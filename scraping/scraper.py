eb7c61081994909f42a7b22f0870e90d79e9507b
EvanShui <evanshui0@gmail.com>
2018-10-31 22:11:45 -0700

    acan import across multiple scripts / insert sys paths

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,5 +1,9 @@
-def main():
+from scraping.twitter_scraper import scrape_tweets
+
+def scraper():
     print('scraping')
+    print('scraping tweets')
+    scrape_tweets()
 
 if __name__ == '__main__':
-    main()
+    scraper()
--- scraping/twitter_scraper.py
+++ scraping/twitter_scraper.py
@@ -1,7 +1,15 @@
+# scraping tweets from Twitter
 import tweepy
+# inserting path to look for keys
+import sys
+sys.path.insert(0, '../../data_301_project/')
+import keys.api_keys
 
-def main():
+print('successfully imported api keys file')
+
+def scrape_tweets():
+    #auth = tweepy.OAuthHandler(twitter_consumer, twitter_consumer_secret)
     print('hello world')
 
 if __name__ == '__main__':
-    main()
+    scrape_tweets()
