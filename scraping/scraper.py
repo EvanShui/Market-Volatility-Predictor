8b972fd7b3f5d002912c60c81d936898667bc694
EvanShui <evanshui0@gmail.com>
2018-11-15 23:33:02 -0800

    added keyword scraping

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, '../../data_301_project')
 from scraping.twitter_scraper import scrape_tweets
 
 def scraper():
--- scraping/twitter_scraper.py
+++ scraping/twitter_scraper.py
@@ -3,13 +3,23 @@ import tweepy
 # inserting path to look for keys
 import sys
 sys.path.insert(0, '../../data_301_project/')
-import keys.api_keys
+from keys.credentials import *
+from pprint import pprint
 
 print('successfully imported api keys file')
 
 def scrape_tweets():
-    #auth = tweepy.OAuthHandler(twitter_consumer, twitter_consumer_secret)
-    print('hello world')
+    auth = tweepy.OAuthHandler(app_key, app_secret)
+    auth.set_access_token(access_token, access_secret)
+    print('confirmed authentication')
+    api = tweepy.API(auth)
+    print('setting authentication')
+    for tweet in tweepy.Cursor(api.search, q='#brazil', count=50, lang='en',since='2017-04-03').items():
+        print("\n----------------\n")
+        print(type(tweet))
+        # Tweet status provides entity recognition. (just presence though).
+        print(tweet._json['text'])
+        # pprint(tweet)
 
 if __name__ == '__main__':
     scrape_tweets()
