8f1b88328f30b8f90fb127f5c8665275a64043cc
Evan Shui <evanshui0@gmail.com>
2018-12-26 02:13:32 -0800

    Delete credentials.py

--- keys/credentials.py
+++
@@ -1,4 +0,0 @@
-app_key = "Pv7rlkvsxwg4kIibmY5eVN917"
-app_secret = "RjhQaZ8uvc8FnvWsGb1sQMRW4pAsMama6sT4iy5MdXugJd3jVb"
-access_token="900992490446114817-V4tQiNVp5UBUTWsWysusmBCk7XcjXOG"
-access_secret="285gUyjU9CKeFsTP92SEScemr6RNu5nZy9UFnAFREOO0h"
